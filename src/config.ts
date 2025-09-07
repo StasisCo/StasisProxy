06d900eaf9f4a782bacd8686e769c09c04c45564
TehPicix <teh.picix@gmail.com>
2025-09-07 01:45:32 -0400

    fix: update chat command handling to use DISABLE_CHAT_COMMANDS for improved control

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 /**
  * The amount of totems to make sure we always have on hand
  * For example, if this is set to 3, the bot will disconnect if it has 3 or less totems and is low on health
- * Set this to -1 to disable auto disconnecting
+ * Set this to -1 to disable auto disconnecting based on totem count
  * @default 3
  */
 export const TOTEM_BUFFER = parseInt(process.env.TOTEM_BUFFER || "3");
@@ -9,7 +9,7 @@ export const TOTEM_BUFFER = parseInt(process.env.TOTEM_BUFFER || "3");
 /** 
  * The health threshold to trigger auto disconnect if we have low totems
  * For example, if this is set to 8, the bot will disconnect if it has 3 or less totems and 8 or less health
- * Set this to -1 to disable auto disconnecting
+ * Set this to -1 to disable auto disconnecting based on health
  * @default 8
  */
 export const HEALTH_BUFFER = parseInt(process.env.HEALTH_BUFFER || "8");
@@ -60,4 +60,4 @@ export const MAX_TRAPDOOR_DISTANCE = parseInt(process.env.MAX_TRAPDOOR_DISTANCE 
  * If this is set to false, the bot will only respond to private messages (whispers)
  * @default true
  */
-export const ENABLE_CHAT_COMMANDS = process.env.ENABLE_CHAT_COMMANDS === undefined ? true : Boolean(process.env.ENABLE_CHAT_COMMANDS);
+export const DISABLE_CHAT_COMMANDS = Boolean(process.env.DISABLE_CHAT_COMMANDS);
