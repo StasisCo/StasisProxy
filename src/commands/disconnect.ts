b6d4151d70e4e564b54a48ecb4492950e1ef6715
TehPicix <teh.picix@gmail.com>
2025-09-16 22:21:16 -0400

    feat: add disconnect and restart commands for bot management

---
+++ src/commands/disconnect.ts
@@ -0,0 +1,24 @@
+import type { Player } from "mineflayer";
+import { Bot } from "../class/Bot";
+import { Logger } from "../class/Logger";
+import { formatPlayer, printObject } from "../utils/format";
+
+export const aliases = [ "disconnect", "dc", "exit" ];
+
+export const permission = "operator";
+
+/**
+ * Disconnect and exit the bot
+ */
+export default async function(player: Player) {
+
+	Bot.instance.chat(`/msg ${ player.username } Disconnecting...`);
+	Logger.log("Disconnecting per user request:");
+	printObject({
+		from: formatPlayer(player)
+	});
+
+	Bot.instance.quit();
+	process.exit(0);
+
+}
---
+++ src/commands/restart.ts
@@ -0,0 +1,24 @@
+import type { Player } from "mineflayer";
+import { Bot } from "../class/Bot";
+import { Logger } from "../class/Logger";
+import { formatPlayer, printObject } from "../utils/format";
+
+export const aliases = [ "restart", "reconnect" ];
+
+export const permission = "operator";
+
+/**
+ * Disconnect and exit the bot with a non-zero exit code to trigger a restart
+ */
+export default async function(player: Player) {
+
+	Bot.instance.chat(`/msg ${ player.username } Restarting...`);
+	Logger.log("Restarting per user request:");
+	printObject({
+		from: formatPlayer(player)
+	});
+
+	Bot.instance.quit();
+	process.exit(-1);
+
+}
