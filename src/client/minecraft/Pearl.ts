011d5c0827192d9cc7631d059f5da4289f7287c4
TehPicix <teh.picix@gmail.com>
2026-05-08 23:46:19 -0400

    refactor: Update import statement for redis to use the correct path

--- src/client/minecraft/Pearl.ts
+++ src/client/minecraft/Pearl.ts
@@ -1,9 +1,9 @@
-import { redis } from "bun";
 import EventEmitter from "events";
 import type { Entity } from "prismarine-entity";
 import { Vec3 } from "vec3";
 import { MinecraftClient } from "~/client/minecraft/MinecraftClient";
 import { StasisManager } from "~/client/minecraft/manager/StasisManager";
+import { redis } from "~/redis";
 import { Stasis } from "./Stasis";
 
 export class Pearl extends EventEmitter<{
